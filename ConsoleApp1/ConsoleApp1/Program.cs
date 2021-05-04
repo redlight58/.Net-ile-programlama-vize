505c9e41e63bbf0c474092bcccd92c2eb0d31691
redlight58 <75634561+redlight58@users.noreply.github.com>
2021-05-04 23:48:19 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ConsoleApp1
+{
+    class two
+    {
+        public static void Main()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            process.StandardInput.WriteLine("netstat -a -n");
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            // process.WaitForExit();
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+            Console.ReadKey();
+        }
+    }
+}
